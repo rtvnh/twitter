886bce003f40d316cb9862599f25abdc0015a9ee
willem-daems <willem.daems@rtvnh.nl>
2014-07-16 16:18:47 +0200

    rebuild

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ class Module
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    'Portal5' . __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
         );
