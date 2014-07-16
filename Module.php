bca8789591f12fe62e69f6b249064b64d0ea8d54
willem-daems <willem.daems@rtvnh.nl>
2014-07-16 15:34:18 +0200

    Removed getConfig()

--- Module.php
+++ Module.php
@@ -4,11 +4,6 @@ namespace Rtvnh;
 
 class Module
 {
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
     public function getAutoloaderConfig()
     {
         return array(
